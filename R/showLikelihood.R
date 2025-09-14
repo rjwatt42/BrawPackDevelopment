f6b20066c0216ec89b0125587feb68be6a33c271
rjwatt42 <r.j.watt42@hotmail.co.uk>
2025-09-14 20:46:13 +0100

    Update showLikelihood.R

--- R/showLikelihood.R
+++ R/showLikelihood.R
@@ -1,6 +1,6 @@
 
 
-showLikelihood<-function(rs=braw.res$result$rIV,showType="mean(R+)",
+showLikelihood<-function(rs=braw.res$result$rIV,n=NULL,showType="mean(R+)",
                          world=braw.def$hypothesis$effect$world,design=braw.def$design,
                          fontsize=1,markRs=NULL,
                          plotArea=c(0,0,1,1),g=NULL,new=TRUE) {
@@ -13,18 +13,26 @@ showLikelihood<-function(rs=braw.res$result$rIV,showType="mean(R+)",
          "Gamma"={PDF<-GammaSamplingPDF},
          "GenExp"={PDF<-GenExpSamplingPDF}
   )
+  if (is.null(n)) n<-design$sN
   if (world$RZ=="z") rs<-atanh(rs)
+  if (length(n)<length(rs)) n<-rep(n,length(rs))
+  
   switch(showType,
     "mean(R+)"={
-      range<-seq(0,0.6,0.01)
+      range<-seq(0,0.6,length.out=201)
       xlabel<-showType
       dens<-0
-      for (i in 1:length(rs))
-        dens<-dens+log(PDF(rs[i],range,1/sqrt(design$sN-3))$pdf)
+      for (i in 1:length(rs)) {
+        nextDens<-PDF(rs[i],range,1/sqrt(n[i]-3))$pdf
+        if (any(nextDens>0))
+          dens<-dens+log(nextDens)
+      }
     }
   )
-  use<-which.max(dens)
+  dens[is.infinite(dens)]<-NA
   dens<-exp(dens)
+  dens[is.na(dens)]<-0
+  use<-which.max(dens)
   
   xlim<-c(min(range),max(range))
   ylim<-c(min(dens),max(dens))+c(-1,1)*(max(dens)-min(dens))*0.2
