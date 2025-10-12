b1582c9b20ffb062b06ecef9e94e0ce2356b5148
rjwatt42 <r.j.watt42@hotmail.co.uk>
2025-10-12 20:19:52 +0100

    Update HTMLWidget.R

--- R/HTMLWidget.R
+++ R/HTMLWidget.R
@@ -132,7 +132,7 @@ generate_tab = function(title="Tab",tabs=c("1","2","3"),tabContents=c("a","b","c
     'display: none;',
     'padding: 0px 0px;',
     'margin: 0px;',
-    'background-color: ','#fa0',';',
+    'background-color: ','#ffffff',';',
     'border: 1px solid ',colours[3],';','border-top: none;',
     'width: ',width,'px;',
     ht,
