abb2a9c72a5bff50f77593e538c0258cabbbcc73
rjwatt42 <r.j.watt42@hotmail.co.uk>
2026-04-12 12:42:27 +0100

    Update showHTML.R

--- R/showHTML.R
+++ R/showHTML.R
@@ -1,20 +1,22 @@
 #' @export
 showHTML<-function(data,new=FALSE) {
+  dir <- tempfile()
+  dir.create(dir)
   if (is.character(data)) {
     if (new) {
       for (i in 1:100) {
-      htmlFile <- file.path(".", paste0("index",i,".html"))
+      htmlFile <- file.path(dir, paste0("index",i,".html"))
       if (!file.exists(htmlFile)) break;
       }
     } else
-      htmlFile <- file.path(".", "index.html")
+    htmlFile <- file.path(dir, "index.html")
     writeLines(data, con = htmlFile)
     rstudioapi::viewer(htmlFile)
   } else {
     assign("graphHTML",TRUE,braw.env)
     show<-data()
     
-    htmlFile <- file.path(".", "index.html")
+    htmlFile <- file.path(dir, "index.html")
     writeLines(show, con = htmlFile)
     rstudioapi::viewer(htmlFile)
     
